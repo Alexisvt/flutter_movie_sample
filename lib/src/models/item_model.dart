2aebf7d6a980181aac32dc4e3e180925bcb3aed5
Alexis Villegas Torres <alexisvt@gmail.com>
2019-04-14 16:25:42 -0600

    fixing some issues

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -39,8 +39,8 @@ class _Result {
   String _overview;
   String _release_date;
 
-  _Result(Map<String, dynamic> result) {
-    _vote_average = result['vote_count'];
+  _Result(result) {
+    _vote_count = result['vote_count'];
     _id = result['id'];
     _video = result['video'];
     _vote_average = result['vote_average'];
@@ -49,7 +49,7 @@ class _Result {
     _poster_path = result['poster_path'];
     _original_language = result['original_language'];
     _original_title = result['original_title'];
-    for (var i = 0; i < result['genre_ids']; i++) {
+    for (int i = 0; i < result['genre_ids'].length; i++) {
       _genre_ids.add(result['genre_ids'][i]);
     }
     _backdrop_path = result['backdrop_path'];
@@ -59,17 +59,29 @@ class _Result {
   }
 
   String get release_date => _release_date;
+
   String get overview => _overview;
+
   bool get adult => _adult;
+
   String get backdrop_path => _backdrop_path;
+
   List<int> get genre_ids => _genre_ids;
+
   String get original_title => _original_title;
+
   String get original_language => _original_language;
+
   String get poster_path => _poster_path;
+
   double get popularity => _popularity;
+
   String get title => _title;
+
   double get vote_average => _vote_average;
+
   bool get video => _video;
+
   int get id => _id;
 
   int get vote_count => _vote_count;
